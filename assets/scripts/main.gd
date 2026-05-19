01f61ec952ce9de2d56c98ada1bc5302533ac819
dykesimulator <dykesimulator@gmail.com>
2026-05-19 15:45:21 +0800

    WE ADDED HIGH SCORE + GAME OVER + YOU CAN SAVE NOW

--- assets/scripts/main.gd
+++ assets/scripts/main.gd
@@ -1,5 +1,8 @@
 extends Node
 
+#for saving
+var save_path = "user://variable.save"
+
 #preload obstacles
 var stump = preload("res://assets/stump.tscn");
 var rock = preload("res://assets/rock.tscn");
@@ -16,12 +19,15 @@ var difficulty: int = 0;
 const MAX_DIFFICULTY: int = 5
 const bird_difficulty_spawn: int = 3;
 
-var score: int
+var score: int;
+var high_score: int;
 const SCORE_MODIFIER: int = 50;
 @onready var dino: CharacterBody2D = $dino;
 @onready var camera: Camera2D = $camera
 @onready var ground: StaticBody2D = $ground
 @onready var hud: CanvasLayer = $HUD
+@onready var gameover: CanvasLayer = $GAMEOVER
+var player: CharacterBody2D;
 
 var speed: int; #changing speed
 const START_SPEED: int = 10;
@@ -35,14 +41,20 @@ var obs_spacing_max: int = 500;
 var obs_x_offscreen = 100; #so it will appear offscreen
 
 func _ready():
+	player = get_tree().get_first_node_in_group("player")
 	screen_size = get_window().size;
 	ground_height = ground.get_node("Sprite2D").texture.get_height()
+	gameover.get_node("restart2").pressed.connect(new_game) #when the restart button is pressed the game will restart
 	new_game()
 
 func new_game():
+	load_data()
 	#reset var
 	score = 0;
 	show_score();
+	#show high score
+	check_high_score()
+	get_tree().paused = false; #SO IF There is a gameover the game restarts and if we restart the gameunpauses
 	difficulty = 0;
 	#resets everything if there is a new game
 	dino.position = DINO_START_POS;
@@ -50,8 +62,14 @@ func new_game():
 	camera.position = CAM_START_POS;
 	ground.position = Vector2i(0,0)
 	
-	#show start game text
+	#resets obstacles
+	for obs in obstacles:
+		obs.queue_free()
+	obstacles.clear()
+	
+	#show start game text + hiding game over text
 	hud.get_node("MarginContainer/pressspaceto_play").show()
+	gameover.hide()
 
 func _process(_delta):
 	if game_running == true:
@@ -77,7 +95,7 @@ func _process(_delta):
 		
 		#remove obstacles that have gone off screen
 		for obs in obstacles:
-			if obs.position < (camera.position.x - screen_size.x):
+			if obs.position.x < (camera.position.x - screen_size.x):
 				remove_obs(obs)
 	else:
 		if Input.is_action_just_pressed("jump"):
@@ -109,6 +127,7 @@ func generate_obs():
 
 func add_obs(obs, x, y): #adds obstacles
 	obs.position = Vector2i(x, y)
+	obs.body_entered.connect(hit_obs)
 	add_child(obs);
 	obstacles.append(obs)
 
@@ -116,12 +135,42 @@ func remove_obs(obs): #removes obstacles from the node tree AND array
 	obs.queue_free()
 	obstacles.erase(obs)
 
+func hit_obs(body): #when you hhit  the obstacle
+	if body == player: 
+		game_over()
+
 func show_score():
 	@warning_ignore("integer_division")
 	hud.get_node("MarginContainer/current_score").text = str(score / SCORE_MODIFIER)
+
+func check_high_score():
+	if score > high_score:
+		high_score = score;
+	@warning_ignore("integer_division")
+	hud.get_node("MarginContainer/HIGHESTSCORE").text = str(high_score / SCORE_MODIFIER)
 
 func adjust_difficulty():
 	@warning_ignore("narrowing_conversion")
 	difficulty = score / SPEED_MODIFIER;
 	if difficulty > MAX_DIFFICULTY:
 		difficulty = MAX_DIFFICULTY;
+
+func game_over(): #ends the game
+	save()
+	get_tree().paused = true;
+	game_running = false;
+	gameover.show()
+
+#for saving the file
+func save():
+	var file = FileAccess.open(save_path, FileAccess.WRITE)
+	file.store_var(high_score)
+
+#for loading the save data
+func load_data():
+	if FileAccess.file_exists(save_path):
+		var file = FileAccess.open(save_path, FileAccess.READ)
+		high_score = file.get_var(high_score); #gets the var saved in the file
+	else:
+		print("no data found!")
+		high_score = 0;
